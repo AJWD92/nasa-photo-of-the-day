ed02c02d24be1d904a2bf100a3aac2a45d271e68
Aaron-Doran <wesley.gossett@gmail.com>
2019-11-07 21:05:56 -0500

    Finished MVP. Having trouble getting background img to display correctly.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NasaList from "./components/NasaList";
-
+import NasaBackground from "./assets/NasaBackground.jpg"
 import "./App.css";
+    
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ background: "#fff" }}>
+  <img className="spacePhoto" alt="Galxey" src={NasaBackground}/>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role='img' aria-label='rocketship'>🚀</span>!
--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardTitle } from 'reactstrap';
+
 
 const NasaCard = props => {
     return (
-        <div className='photoOfTheDay'>
-            <img className='photo' alt='nasa' src={props.url} />
-            <h1>Photo of: {props.title}</h1>
-            <p>Date taken: {props.date}</p>
-            <p>Explanation: {props.explanation}</p>
-
-
-        </div>
+        <Card style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '600px', height: '650px', margin: '0 auto', position: 'relative' }}>
+            <CardImg alt='nasa' src={props.url} style={{ maxWidth: '250px', maxHeight: '250px', }} />
+            <CardTitle>Photo of: {props.title}</CardTitle>
+            <CardText>Date taken: {props.date}</CardText>
+            <CardText>Explanation: {props.explanation}</CardText>
+        </Card>
     )
 }
 
